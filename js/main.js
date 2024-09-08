ff012f0dd3c2d34c43f0dcf785d1bf01fea5771b
lehanhdev <lehanhdev@gmail.com>
2024-09-08 19:36:07 +0700

    Change date

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function () {
 
 
 $('#countdown').countdown({
-	date: "September 02 2024",
+	date: "December 26 2024",
 	render: function (data) {
 		var el = $(this.el)
 		el.empty()
